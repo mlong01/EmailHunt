61c97b5b5c3955b82a2841e25fbdbab05406d7a3
mlong01 <minnvik98151@gmail.com>
2015-10-22 00:07:42 -0400

    app takes url from command line

--- scrape.py
+++ scrape.py
@@ -1,3 +1,16 @@
 import requests
+import sys
 
-r = requests.get("http://jana.com")
+if len(sys.argv) != 2:
+    print "USAGE: python scrape.py [url]"
+    sys.exit(0)
+
+# Format so requests recognizes
+url = "http://" + sys.argv[1]
+
+try:
+    r = requests.get(url)
+    print r.content
+except:
+    print "Domain could not be accessed"
+
