50ac7d76b0cdb24783a77b1a4fe6d52af99884d8
mlong01 <minnvik98151@gmail.com>
2015-10-26 11:05:44 -0400

    refactored into class to accomodate multiple webpages

--- scrape_class.py
+++ scrape_class.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import re
 from PyQt4.QtGui import *  
 from PyQt4.QtCore import *  
@@ -8,26 +8,60 @@ from PyQt4.QtWebKit import *
 EMAIL_REGEX = '(\w+(\.\w*)*@\w+(\.\w*)+)'
 
 # Used to find relative links in html body
-RELATIVE_LINK_FIND_REGEX = 'href="\/.+"'
+RELATIVE_LINK_FIND_REGEX = 'href="\/[^"]+"'
 RELATIVE_LINK_CLEAN_REGEX = '\/[^"]+'
 
 # TODO: regex for finding absolute links
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # Class that renders a webpage, allowing JS elements to load
 # Code for this class was pulled from from 
 #       webscraping.com/blog/Scraping-JavaScript-webpages-with-webkit/
 # Module is licenced under LGPL, so I'm allowed to use it
 class Render(QWebPage):  
-  def __init__(self, url):  
+  def __init__(self, urls, cb):
     self.app = QApplication(sys.argv)  
     QWebPage.__init__(self)  
     self.loadFinished.connect(self._loadFinished)  
-    self.mainFrame().load(QUrl(url))  
+    self.urls = urls  
+    self.cb = cb
+    self.visited = {}
+    self.crawl()  
     self.app.exec_()  
-  
+      
+  def crawl(self):  
+    if self.urls:  
+      url = self.urls.pop(0)  
+      self.mainFrame().load(QUrl(url))  
+    else:  
+      self.app.quit()  
+        
   def _loadFinished(self, result):  
-    self.frame = self.mainFrame()  
-    self.app.quit() 
+    frame = self.mainFrame()  
+    url = str(frame.url().toString())  
+    html = frame.toHtml()  
+    
+    emails, urls = self.cb(url, html)
+
+    for email in emails:
+        print email
+
+    for url in urls:
+        if not url[1] in self.visited:
+            self.visited[url[1]] = 'visited'
+            self.urls.append(url[0])
+    
+    self.crawl()  
+
+
+# Function that, when given a URL, finds and prints the email addresses
+# on the website
+def scrape_and_search(url, html):
+    emails = pull_emails(html)
+    urls = pull_links(url, html)
+
+    return emails, urls
 
 
 # Purpose:   when given a body of text (such as HTML), return a list of
@@ -54,53 +88,21 @@ def pull_emails(body):
 # Note:      Currently determines websites 'in the domain' by looking for
 #            relative paths in href element. Does not work for any absolute
 #            paths in body
-def pull_links(body, base_url):
+def pull_links(base_url, body):
     matches = re.findall(re.compile(RELATIVE_LINK_FIND_REGEX), body)
     links = []
     
     for m in matches:
-        rel_path = re.search(re.compile(RELATIVE_LINK_CLEAN_REGEX), m).group()
-        to_append = base_url + rel_path
-        links.append(to_append)
-    
+        rel_path = re.search(re.compile(RELATIVE_LINK_CLEAN_REGEX), m)
+   
+        if rel_path:
+            rel_path = rel_path.group()
+            to_append = base_url + rel_path[1:]  # remove leading /
+            links.append((to_append, rel_path))
+        
     return links
 
-# Function that, when given a URL, finds and prints the email addresses
-# on the website
-def scrape_and_search(base):
-    # Stack to keep track of pages to possibly visit
-    pages = [base]
 
-    # Dict to keep track of pages already visited
-    visited = {}
-
-    while pages:
-        print "Pages: "
-        print pages
-        url = pages.pop()
-
-        #try:
-        r = Render(url)
-        html = r.frame.toHtml()
-
-        print "well hi there"
-        
-        # need to check url actually matched
-        # can compare to <html><head></head><body></body></html>
-        # if not, continue
-
-        emails = pull_emails(html)
-        for email in emails:
-            print "HERE'S AN EMAIL:    " + email
-
-        new_links = pull_links(html, url)
-        for link in new_links:
-            if not link in visited:
-                visited[link] = 'visited'
-                pages.append(link)
-
-        #except:
-        #    print "Something went wrong"
 
 #################################################
 
@@ -108,7 +110,7 @@ if len(sys.argv) != 2:
     print "USAGE: python scrape.py [url]"
     sys.exit(0)
 
-# Format so requests recognizes
-url = "http://" + sys.argv[1]
-scrape_and_search(url)
+urls = ["http://" + sys.argv[1]]
+
+r = Render(urls, cb=scrape_and_search)
 
