6d4e21b937be2019396a1fec109325a7934aba35
mlong01 <minnvik98151@gmail.com>
2015-10-25 23:29:45 -0400

    command line interface and email regex added but still has issue with JS-rendered content

--- scrape.py
+++ scrape.py
@@ -1,6 +1,10 @@
 import requests
 import sys
 
+# Any num of non-whitespace chars followed by @ followed by a word
+# followed by a '.' followed by another word
+EMAIL_REGEX = '\S*@\w*.\w*'
+
 if len(sys.argv) != 2:
     print "USAGE: python scrape.py [url]"
     sys.exit(0)
@@ -11,6 +15,7 @@ url = "http://" + sys.argv[1]
 try:
     r = requests.get(url)
     print r.content
+    # ISSUE - does not work on JS-rendered sites
 except:
     print "Domain could not be accessed"
 
