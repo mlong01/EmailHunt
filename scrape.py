12fc921c69f90cea1c327754c8aee8d42c07e424
mlong01 <minnvik98151@gmail.com>
2015-10-26 05:27:02 -0400

    code refactoring

--- scrape.py
+++ scrape.py
@@ -4,12 +4,13 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *  
 from PyQt4.QtWebKit import *  
 
-# Any num of non-whitespace chars followed by @ followed by a word
-# followed by a '.' followed by another word
-EMAIL_REGEX = '\S*@\w*.\w*'
-NEW_REGEX = '(\w+(\.\w*)*@\w+(\.\w*)+)'
+# Regular expressions for pattern matching
+EMAIL_REGEX = '(\w+(\.\w*)*@\w+(\.\w*)+)'
 
-### This class taken from (url plug in later) ###
+# Class that renders a webpage, allowing JS elements to load
+# Code for this class was pulled from from 
+#       webscraping.com/blog/Scraping-JavaScript-webpages-with-webkit/
+# Module is licenced under LGPL, so I'm allowed to use it
 class Render(QWebPage):  
   def __init__(self, url):  
     self.app = QApplication(sys.argv)  
@@ -21,6 +22,68 @@ class Render(QWebPage):
   def _loadFinished(self, result):  
     self.frame = self.mainFrame()  
     self.app.quit() 
+
+
+# Purpose:   when given a body of text (such as HTML), return a list of
+#            all the emails hidden inside
+# Arguments: regex-searchable string (body of webpage)
+# Returns:   list of unique email addresses
+def pull_emails(body):
+    matches = re.findall(re.compile(EMAIL_REGEX), body)
+    emails = []
+    for m in matches:
+        emails.append(m[0])
+
+    emails = list(set(emails))
+
+    return emails
+
+
+# Purpose:   when given a body of text (such as HTML), and a URL, return a 
+#            list of URLs within the given domain that are linked in the
+#            body
+# Arguments: regex-searchable string (body of webpage), string (URL of
+#            webpage passed in as body)
+# Returns:   list of unique URLs linked within the body
+# Note:      Currently determines websites 'in the domain' by looking for
+#            relative paths in href element. Does not work for any absolute
+#            paths in body
+def pull_links(body, base_url):
+    return []
+
+# Function that, when given a URL, finds and prints the email addresses
+# on the website
+def scrape_and_search(base):
+    # Stack to keep track of pages to possibly visit
+    pages = [base]
+
+    # Dict to keep track of pages already visited
+    visited = {}
+
+    while pages:
+        url = pages.pop()
+
+        try:
+            r = Render(url)
+            html = r.frame.toHtml()
+            
+            # need to check url actually matched
+            # can compare to <html><head></head><body></body></html>
+            # if not, continue
+
+            emails = pull_emails(html)
+            for email in emails:
+                print email
+
+            new_links = pull_links(html, url)
+            for link in new_links:
+                if not link in visited:
+                    visited[link] = 'visited'
+                    pages.add(link)
+
+        except:
+            print "Something went wrong"
+
 #################################################
 
 if len(sys.argv) != 2:
@@ -29,24 +92,5 @@ if len(sys.argv) != 2:
 
 # Format so requests recognizes
 url = "http://" + sys.argv[1]
-
-try:
-    r = Render(url)
-    html = r.frame.toHtml()
-
-    # need to check url actually matched
-    # can compare to <html><head></head><body></body></html>
-
-    matches = re.findall(re.compile(NEW_REGEX), html)
-    emails = []
-    for m in matches:
-        emails.append(m[0])
-
-    emails = list(set(emails))
-
-    for e in emails:
-        print e
-
-except:
-    print "Something went wrong"
+scrape_and_search(url)
 
